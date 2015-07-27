8f88f41e5e204e94f74eea924bbc1f91fec45b7e
hollandz <hollandz@icloud.com>
2015-07-26 20:49:45 -0400

    emailFunctionalityAdded

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-﻿$(document).foundation();
+﻿
+
+$(document).foundation();
 
 var parent = $("#links");
 var links = parent.children();
@@ -15,10 +17,10 @@ blueimp.Gallery(document.getElementById('links').getElementsByTagName('a'),
 
 angular.module('fab', []);
 angular.module('fab')
-
     .controller('ContactController', ['$scope', '$http', function ($scope, $http) {
         $scope.success = false;
         $scope.error = false;
+
         $scope.send = function () {
 
             var htmlBody = '<div>Name: ' + $scope.user.name + '</div>' +
@@ -26,27 +28,36 @@ angular.module('fab')
                            '<div>Message: ' + $scope.user.body + '</div>' +
                            '<div>Date: ' + (new Date()).toString() + '</div>';
 
-            $http({
-                url: 'https://api.postmarkapp.com/email',
-                method: 'POST',
+            $.ajax({
+
+                url: 'https://api.sendgrid.com/api/mail.send.json',
+                type: 'GET',
+                timeout: 5000,
                 data: {
-                    'From': 'fab5sites@web.com',
-                    'To': 'zaphane.holland@gmail.com',
-                    'HtmlBody': htmlBody,
-                    'Subject': 'New Contact Form Submission'
+                    'api_user': 'azure_26671fad870f47d3458dc77dede74dd5@azure.com',
+                    'api_key': '8Ia5zRY04Ow9re9',
+                    'to': 'zaphane.holland@gmail.com',
+                    'toname': 'Work email',
+                    'subject': 'New Contact Form Submission',
+                    'from': $scope.user.email,
+                    'text': $scope.user.body
                 },
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'X-Postmark-Server-Token': '8569dcd45-6a1a-4e7b-ae75-ea37629de4'
+                dataType: 'jsonp',
+                error: function (x, t, m) {
+                    debugger;
+                    if (t === "timeout") {
+                        $scope.error = true;
+                    } else if (t == "parsererror") {
+                        $scope.success = true;
+                        $scope.user = {};
+                        $scope.contactForm.$setPristine();
+                        $scope.$digest();
+                    }
                 }
-            }).
-            success(function (data) {
-                $scope.success = true;
-                $scope.user = {};
-            }).
-            error(function (data) {
-                $scope.error = true;
+
+
             });
-        }
-    }]);
+
+
+        }// end scope.send
+    }]);//end conroller definition
