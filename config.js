a2216bdee95da38958e1f19e49d97299a988c01d
Zae Holland <zaphane.holland@gmail.com>
2015-10-10 17:48:24 -0400

    -image optimized :) -node essentials updated

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
   sendgrid: {
-    user: process.env.SENDGRID_USER || '',
-    pass: process.env.SENDGRID_PASSWORD || '',
+    user: process.env.SENDGRID_USER || 'azure_26671fad870f47d3458dc77dede74dd5@azure.com',
+    pass: process.env.SENDGRID_PASSWORD || '8Ia5zRY04Ow9re9',
     to: process.env.EMAIL_RECIPIENT || 'zaphane.holland@gmail.com'
   }
 };
